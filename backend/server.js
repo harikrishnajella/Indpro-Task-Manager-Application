5d479e9475560ccc6bef21f0f417c663832b779c
jellaharikrishna <rk40665@gmail.com>
2025-03-25 09:47:37 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("dotenv").config();
 
 const userAuthRoute = require("./routes/userAuthRoute");
 const taskRoute = require("./routes/taskRoute");
@@ -20,7 +20,7 @@ app.use("/api/tasks", taskRoute);
 app.use("/api", userProfile)
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB is connected successfully..."))
+  .then(() => console.log("MongoDB is connected successfully...."))
   .catch(err => console.log(err));
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}....`))
